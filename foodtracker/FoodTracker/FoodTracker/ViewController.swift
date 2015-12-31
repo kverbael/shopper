66181fe22f4612260a88ce147c17ad46f9a04227
Kevin  Verbael <kevin.verbael14@imperial.ac.uk>
2015-12-30 16:41:29 -0800

    Basic iOS tutorial project built

---
+++ foodtracker/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kevin Verbael on 12/30/15.
+//  Copyright © 2015 Kevin and Kyle Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
